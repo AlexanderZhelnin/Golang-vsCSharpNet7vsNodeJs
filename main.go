3b0344f4c3a1eda00e44927293a79ebf65212b38
SNeiko <s_neiko@outlook.com>
2023-04-16 14:59:23 +0100

    add fiber route and sonic marshal/unmarshal

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"github.com/bytedance/sonic"
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
 	"strconv"
 )
+
+type MapStructCoord struct {
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
+}
 
 var ls []Legend
 var rect = Rect{Left: 1200, Bottom: 50, Right: 4000, Top: 2850}
@@ -16,106 +23,85 @@ var STR1 = "asrgfsadf12421"
 var STR2 = "asrgfsadf12321"
 
 func main() {
+	router := fiber.New(fiber.Config{
+		JSONEncoder: sonic.Marshal,
+		JSONDecoder: sonic.Unmarshal,
+	})
 
-	plan, _ := ioutil.ReadFile("primitives.json")
+	plan, _ := os.ReadFile("primitives.json")
 
 	err := json.Unmarshal(plan, &ls)
 	if err != nil {
 		fmt.Print("Не могу прочитать json ", err)
 	}
 
-	// fmt.Println(data)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World golang!")
+	router.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World golang!")
 	})
 
-	http.HandleFunc("/map", func(w http.ResponseWriter, r *http.Request) {
+	router.Get("/map", func(ctx *fiber.Ctx) error {
+		var q MapStructCoord
+		if err := ctx.QueryParser(&q); err != nil {
+			return err
+		}
 
-		query := r.URL.Query()
-		xStr := query["x"][0]
-		yStr := query["y"][0]
-
-		x, _ := strconv.ParseFloat(xStr, 64)
-		y, _ := strconv.ParseFloat(yStr, 64)
-
-		x /= 100
-		y /= 100
+		q.X /= 100
+		q.Y /= 100
 
 		resultLs := build(ls,
-			DrawPr{Left: pr.Left + x, Top: pr.Top + y, Scale: pr.Scale, Mashtab: pr.Mashtab},
+			DrawPr{Left: pr.Left + q.X, Top: pr.Top + q.Y, Scale: pr.Scale, Mashtab: pr.Mashtab},
 			Rect{
-				Left:   rect.Left + x,
-				Top:    rect.Top + y,
+				Left:   rect.Left + q.X,
+				Top:    rect.Top + q.Y,
 				Right:  rect.Right,
 				Bottom: rect.Bottom})
-
-		// b, err := json.Marshal(resultLs)
-
-		// if err != nil {
-		// 	fmt.Print("Не могу сохранить json ", err)
-		// }
-
-		// w.Header().Set("Content-Type", "application/json")
-		// result := strconv.Itoa(len(b))
-
-		// buf := make([]byte, len(result))
-		// copy(buf, result)
-		// w.Write(buf)
-
-		// //w.Write(b)
 
 		result := strconv.Itoa(len(resultLs))
 
 		buf := make([]byte, len(result))
 		copy(buf, result)
-		w.Write(buf)
+		return ctx.JSON(buf)
 	})
 
-	http.HandleFunc("/mapJSON", func(w http.ResponseWriter, r *http.Request) {
+	router.Get("/mapJSON", func(ctx *fiber.Ctx) error {
+		var q MapStructCoord
+		if err := ctx.QueryParser(&q); err != nil {
+			return err
+		}
 
-		query := r.URL.Query()
-		xStr := query["x"][0]
-		yStr := query["y"][0]
-
-		x, _ := strconv.ParseFloat(xStr, 64)
-		y, _ := strconv.ParseFloat(yStr, 64)
-
-		x /= 100
-		y /= 100
+		q.X /= 100
+		q.Y /= 100
 
 		resultLs := build(ls,
-			DrawPr{Left: pr.Left + x, Top: pr.Top + y, Scale: pr.Scale, Mashtab: pr.Mashtab},
+			DrawPr{Left: pr.Left + q.X, Top: pr.Top + q.Y, Scale: pr.Scale, Mashtab: pr.Mashtab},
 			Rect{
-				Left:   rect.Left + x,
-				Top:    rect.Top + y,
+				Left:   rect.Left + q.X,
+				Top:    rect.Top + q.Y,
 				Right:  rect.Right,
 				Bottom: rect.Bottom})
 
-		b, err := json.Marshal(resultLs)
+		b, err := sonic.Marshal(resultLs)
 
 		if err != nil {
 			fmt.Print("Не могу сохранить json ", err)
 		}
 
-		w.Header().Set("Content-Type", "application/json")
 		result := strconv.Itoa(len(b))
 
-		buf := make([]byte, len(result))
-		copy(buf, result)
-		w.Write(buf)
-
-		//w.Write(b)
+		return ctx.SendString(result)
 	})
 
-	http.HandleFunc("/naturalsort", func(w http.ResponseWriter, r *http.Request) {
+	router.Get("/naturalsort", func(ctx *fiber.Ctx) error {
 		result := 0
-		for i := 0; i < 10000; i++ {
+		for i := 0; i < 10_000; i++ {
 			result += Compare(STR1+strconv.Itoa(i), STR2+strconv.Itoa(i))
 		}
 
-		fmt.Fprint(w, result)
+		fmt.Println(result)
+		return ctx.JSON(result)
 	})
 
-	http.ListenAndServe("localhost:4000", nil)
+	if err := router.Listen(":4000"); err != nil {
+		log.Fatal(err)
+	}
 }
